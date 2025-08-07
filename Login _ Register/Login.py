59d94bcc72f32211e874d29a293284c97f4be79e
Rupesh2001 <rmmahat2010@gmail.com>
2025-08-07 11:11:35 +0545

    Updated in if function:

--- Login _ Register/Login.py
+++ Login _ Register/Login.py
@@ -2,24 +2,51 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pytest
 import time
+import faker
+from faker import Faker
 
 @pytest.fixture(scope="function")
 def driver():
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
     driver.maximize_window()
+    driver.implicitly_wait(10)
     yield driver
     driver.quit()
 
 def test_login(driver):
     try:
         driver.get("https://waitermoduleapp.danfesolution.com/login")
-        time.sleep(2)  # Consider using WebDriverWait for better stability
-        driver.save_screenshot("Pass_login_page.png")
+        time.sleep(5)
+        faker = Faker()
+        email = driver.find_element(By.XPATH,"//div[@class='p-2 py-4']//input[@placeholder='Enter email']")
+        email.send_keys(faker.email())
+        time.sleep(2)
+        password = driver.find_element(By.XPATH,"//div[@class='p-2 py-4']//input[@placeholder='Enter Password']")
+        password.send_keys(faker.password())
+        time.sleep(2)
+        Remember_me = driver.find_element(By.XPATH,"//div[@class='d-flex justify-content-between mb-4']//input[@id='auth-remember-check']")
+        remember_me = Remember_me.is_selected()
+        if not remember_me:
+            Remember_me.click()
+        submit = driver.find_element(By.XPATH,"//div[@class='p-2 py-4']//button[@type='submit'][normalize-space()='Sign In']")
+        submit.click()
+        time.sleep(5)
+        Toggle_msg = driver.find_element(By.XPATH,"//div[@role='status']")
+        color = Toggle_msg.value_of_css_property("color")
+        if "rgb(0, 128, 0)" in color or "green" in color:
+             print("success")
+        elif "rgb(255, 0, 0)" in color or "red" in color:
+             print("not success")
+        else:
+            print("Unknown color:", color)
+
     except Exception as e:
         print(f"Error accessing the website: {e}")
-        driver.save_screenshot("Fail_login_page.png")
+        # driver.save_screenshot("Fail_login_page.png")
 
         
